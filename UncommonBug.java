8a0147a5375659305f0ac127fbb0e8b8e20e9705
BugHunter-X <askzenai@gmail.com>
2025-01-25 02:28:07 +0100

    Add UncommonBug.java file

---
+++ UncommonBug.java
@@ -0,0 +1,26 @@
+public class UncommonBug {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > 10) {
+                System.out.println("Sum exceeds 10");
+                break; //This is crucial for this specific bug!
+            }
+        }
+        System.out.println("Final sum: " + sum);
+
+        //Demonstrates a different case of the bug:
+        int[] arr2 = {10, 20, 30};
+        int sum2 = 0;
+        for(int i = 0; i < arr2.length; i++){
+            sum2 += arr2[i];
+            if(sum2 > 50){
+                System.out.println("Sum exceeds 50");
+                break; //Crucial for the second case!
+            }
+        }
+        System.out.println("Final sum2: " + sum2);
+    }
+}
